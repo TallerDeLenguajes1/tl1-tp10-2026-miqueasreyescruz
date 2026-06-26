37223001cac31045890a83104c072931f45c2dd2
MiqueasReyes <miqueasreyescruz@gmail.com>
2026-06-26 20:49:19 -0300

    Agrego a la clase GestorTarea el metodo para llamar a la APi, asi tambien en el program, aplico su uso con una funcion

--- Tareas/GestorTareas.cs
+++ Tareas/GestorTareas.cs
@@ -1,7 +1,47 @@
 namespace Tareas;
 
+using System.Text.Json;
+
 public class GestorTarea
 {
+    // Declaro 2 instancia de listas de tareas para usarlas en un futuo
     List<Tarea> _tareaspendientes = new List<Tarea> ();
     List<Tarea> _tareasrealizadas = new List<Tarea> ();
+
+    // Creo la instancia unica del cliente HttpClient
+    private static readonly HttpClient client = new HttpClient();
+    // Declaro el metodo asincrono, el cual recibe una url (valida) y luego guarda las tareas
+    public async Task<List<Tarea>> GetTareas(string url)
+    {
+        // Realizo la peticion HTTP GET
+        HttpResponseMessage response = await client.GetAsync(url);
+        // Valido el estado de la respuesta
+        response.EnsureSuccessStatusCode();
+        // Leo el cuerpo de la respuesta como texto
+        string body = await response.Content.ReadAsStringAsync();
+        // Deserializacion
+        List<Tarea> tareas = JsonSerializer.Deserialize<List<Tarea>>(body);
+        // Recorro las tareas y las clasifico segun si estan realizadas
+        foreach (var tarea in tareas)
+        {
+            if (tarea.completed == true)
+            {
+                _tareasrealizadas.Add(tarea);
+            }
+            else
+            {
+                _tareaspendientes.Add(tarea);
+            }
+        }
+        return tareas;
+    }
+
+    public IReadOnlyList<Tarea> Pendientes
+    {
+        get => _tareaspendientes.AsReadOnly();
+    }
+    public IReadOnlyList<Tarea> Realizadas
+    {
+        get => _tareasrealizadas.AsReadOnly();
+    }
 }
--- Tareas/Program.cs
+++ Tareas/Program.cs
@@ -1,1 +1,33 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// ========================
+//        ESPACIOS
+// ========================
+
+using Tareas;
+
+// ========================
+//          MAIN
+// ========================
+
+var Gestor = new GestorTarea();
+List<Tarea> paqueteJson = await Gestor.GetTareas("https://jsonplaceholder.typicode.com/todos/");
+
+mostrarTareas("tareas ingresadas",paqueteJson);
+mostrarTareas("tareas pendientes",Gestor.Pendientes);
+mostrarTareas("tareas realizadas",Gestor.Realizadas);
+
+// ========================
+//         FUNCIONES
+// ========================
+
+void mostrarTareas(string titulo, IReadOnlyList<Tarea> lista)
+{
+    Console.WriteLine(new string('=', titulo.Length + 4));
+    Console.WriteLine($"  {titulo.ToUpper()}  ");
+    Console.WriteLine(new string('=', titulo.Length + 4));
+    Console.WriteLine($"[{"ID",3}] [{"uID",3}] [{"ESTADO",-10}] | {"TITULO"}");
+
+    foreach (Tarea tarea in lista)
+    {
+        Console.WriteLine($"[{tarea.id,3}] [{tarea.userId,3}] [{(tarea.completed ? "Completada" : "Pendiente"),-10}] | {tarea.title}");
+    }
+}
