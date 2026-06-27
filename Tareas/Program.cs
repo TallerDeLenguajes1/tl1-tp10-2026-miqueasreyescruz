5db0e9fc267898f3210c3437d8c9d437f665f8f7
MiqueasReyes <miqueasreyescruz@gmail.com>
2026-06-26 21:16:43 -0300

    Agrego la funcion serializarJson

--- Tareas/Program.cs
+++ Tareas/Program.cs
@@ -2,6 +2,7 @@
 //        ESPACIOS
 // ========================
 
+using System.Text.Json;
 using Tareas;
 
 // ========================
@@ -15,10 +16,14 @@ mostrarTareas("tareas ingresadas",paqueteJson);
 mostrarTareas("tareas pendientes",Gestor.Pendientes);
 mostrarTareas("tareas realizadas",Gestor.Realizadas);
 
+string ruta = Directory.GetCurrentDirectory();
+serializarJson(ruta,paqueteJson);
+
 // ========================
 //         FUNCIONES
 // ========================
 
+// Recibe un titulo, una lista, y muestra en pantalla ambas cosas.
 void mostrarTareas(string titulo, IReadOnlyList<Tarea> lista)
 {
     Console.WriteLine(new string('=', titulo.Length + 4));
@@ -30,4 +35,14 @@ void mostrarTareas(string titulo, IReadOnlyList<Tarea> lista)
     {
         Console.WriteLine($"[{tarea.id,3}] [{tarea.userId,3}] [{(tarea.completed ? "Completada" : "Pendiente"),-10}] | {tarea.title}");
     }
+}
+
+// Recibe una ruta, una lista, y crea el aarchivo tareas.json
+void serializarJson(string path, List<Tarea> lista)
+{
+    string rutaFinal = Path.Combine(path,"tareas.json");
+    using StreamWriter escritor = new StreamWriter(rutaFinal);
+
+    string json = JsonSerializer.Serialize(lista);
+    escritor.WriteLine($"{json}");   
 }
